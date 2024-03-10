6d6ed8314d80d01e2272e594bd0ed95ae7c8b169
kmusayeva <khmusayeva@gmail.com>
2024-03-10 13:48:25 +0100

    Add plot confusion matrix

--- soil_microbiome/data_analysis/SpeciesClassification.py
+++ soil_microbiome/data_analysis/SpeciesClassification.py
@@ -9,6 +9,8 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
+import itertools
+
 
 
 from .. import global_vars
@@ -113,6 +115,49 @@ class SpeciesClassification:
 
             self._do_classify(LogisticRegression(), y)
         
+
+
+
+    def plot_confusion_matrix():
+
+
+
+
+    def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+            """
+            This function prints and plots the confusion matrix.
+            Normalization can be applied by setting `normalize=True`.
+            """
+            if normalize:
+                cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+                print("Normalized confusion matrix")
+            else:
+                print('Confusion matrix, without normalization')
+
+            print(cm)
+
+            plt.imshow(cm, interpolation='nearest', cmap=cmap)
+            plt.title(title)
+            plt.colorbar()
+            tick_marks = np.arange(len(classes))
+            plt.xticks(tick_marks, classes, rotation=45)
+            plt.yticks(tick_marks, classes)
+
+            fmt = '.2f' if normalize else 'd'
+            thresh = cm.max() / 2.
+            for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+                plt.text(j, i, format(cm[i, j], fmt),
+                         horizontalalignment="center",
+                         color="white" if cm[i, j] > thresh else "black")
+
+            plt.tight_layout()
+            plt.ylabel('True label')
+            plt.xlabel('Predicted label')
+
+
 
 
     def get_perf(self):
