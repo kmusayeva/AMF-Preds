976c4037cd03b3ca12d3c832050819a4f5a21e7f
kmusayeva <khmusayeva@gmail.com>
2024-07-09 18:54:29 +0200

    latest

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from m_lp.multi_label_propagation import *
-from soil_microbiome.data_process import weather_soil
 from soil_microbiome.data_analysis import *
 
 tax_level = 'Species'
 
-# df = pd.read_excel("./soil_microbiome/data/GlobalAMFungi/Species/sol_Species.xlsx")
+# df = pd.read_excel("./soil_microbiome/data/GlobalAMFungi/Species/Species.xlsx")
 
 soil_usage = ['anthropogenic', 'cropland', 'desert', 'forest', 'grassland', 'shrubland', 'tundra', 'woodland']
 soil_vars = ['pH', 'MAP', 'MAT']
 env_vars = [*soil_usage, *soil_vars]
-species = SpeciesEurope(tax_level=tax_level, x_dim=19, env_vars=env_vars, is_global_amf=True)
+species = SpeciesEurope(tax_level=tax_level, x_dim=24, env_vars=env_vars, is_global_amf=True)
 species.get_top_species(30)
 species.print_info()
 model = MLClassification(species)
-model.evaluate(2)
+model.evaluate(10)
